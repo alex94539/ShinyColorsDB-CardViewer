12593ab7856b8087c7bcbd0e4b98cf6f3bbceee4
euphokumiko <h.k.h94539@gmail.com>
2021-06-14 13:09:48 +0800

    add CardViewer

--- idolViewer/idolViewer.js
+++ idolViewer/idolViewer.js
@@ -5,7 +5,7 @@ const LGI = "list-group-item",
     LG = "list-group";
 
 async function GetIdolUnitList() {
-    const UnitIdolList = (await axios.get("https://api.shinycolors.moe/general/getIdolUnitList")).data;
+    const UnitIdolList = (await axios.get("http://localhost:3000/General/GetIdolUnitList")).data;
 
     const SideBar = document.getElementById("divIdolUnitList");
 
@@ -27,7 +27,7 @@ async function GetIdolUnitList() {
         const UnitDiv = document.createElement("div");
         UnitDiv.classList.add(LG);
         UnitDiv.classList.add(CLP);
-        UnitDiv.id = `unit${k}`
+        UnitDiv.id = `unit${k}`;
 
         for (let m = 0; m < UnitIdolList[k].UnitIdols.length; m++) {
             const IdolEle = document.createElement("a");
@@ -46,13 +46,6 @@ async function GetIdolUnitList() {
     console.log(UnitIdolList, SideBar);
 }
 
-/**
- *              <a href="#" class="list-group-item" data-toggle="collapse" data-target="#unit1">
-                    <i class="glyphicon glyphicon-chevron-right"></i>Illumination Stars
-                </a>
-                <div class="list-group collapse" id="unit1">
-                    <a href="./1" class="list-group-item">櫻木真乃</a>
-                    <a href="./2" class="list-group-item">風野灯織</a>
-                    <a href="./3" class="list-group-item">八宮めぐる</a>
-                </div>
- */
+async function GetIdolInfoByURI() {
+    const CurrentURI = location.pathname.substring(1);
+}
