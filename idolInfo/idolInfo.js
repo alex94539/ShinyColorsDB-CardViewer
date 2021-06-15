126dfc7ba8862be200f7ac687ea550b9fb3e109f
euphokumiko <h.k.h94539@gmail.com>
2021-06-15 21:24:47 +0800

    Update idolInfo.js

--- idolInfo/idolInfo.js
+++ idolInfo/idolInfo.js
@@ -206,7 +206,8 @@ function generateCardList(obj, index) {
     const a = document.createElement("a");
     a.classList.add("nav-link", "pl-2", "pt-1", "pb-1", "list-group-item");
     a.appendChild(document.createTextNode(obj.CardName));
-    a.addEventListener('click', changePicture(obj.BigPic1));
+    a.setAttribute("onclick", `changePicture(${obj.BigPic1})`);
+    //a.addEventListener('click', changePicture());
 
     li.appendChild(a);
 
